244087c471698d8b970c1baa6f36162b99793c63
Razia Sultana Mun <nisridhika@gmail.com>
2022-01-07 00:22:56 +0600

    Update Header.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,24 +9,50 @@ const Header = () => {
     const {user,logOut,signInUsingGoogle}=useAuth();
     return (
         <div className="header">
-            <nav className="navbar sticky-top navbar-expand-lg  bg-dark">
-      <div className="container-fluid fs-5">
-      <a className="navbar-brand text-light fs-4 fw-bold" href="#">
-      <img src={logo} width="75" height="75" className="rounded-circle"/>
-     Heal & Care</a>
-      
-                <NavLink className="navHover" to="/home">Home</NavLink>
-                <NavLink className="navHover" to="/about">About Us</NavLink>
-                <NavLink className="navHover" to="/services">Services</NavLink>
-                <NavLink className="navHover" to="/doctors">Doctors</NavLink>
-                <NavLink className="navHover" to="/contact">Contact</NavLink>
-               
-                {user?.email?
-                <button onClick={logOut} type="button" className="btn btn-light btn-lg fs-5">Logout</button>:
-                <NavLink to="/login">Login</NavLink>}
-                <small className="text-light fs-5">Sign in As:<a href="#login">{user?.displayName}</a></small>
-                </div>
-            </nav> 
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+  <a class="navbar-brand fs-2 fw-bold text-light" href="#"><img src={logo} width="75" height="75" className="rounded-circle"/>Heal&Cure</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item ">
+          <NavLink to="/home" className="navHover nav-link text-light">Home</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink to="/about" className="navHover nav-link text-light" >About Us</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink to="/doctors" className="navHover nav-link text-light" >Doctor</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink to="/services" className="navHover nav-link text-light" >Service</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink to="/contact" className="navHover nav-link text-light" >Contact</NavLink>
+        </li>
+      </ul>
+      <span class="navbar-text">
+      {user.email ? (
+                 <div className="d-flex align-items-center mx-auto">
+                  <img className="m-2" width='42' height='42' src={profile} alt="" srcset="" />
+                     {user?.displayName}
+                   <button
+                     onClick={logOut}
+                     className="btn-secondary rounded me-2 mx-3 py-1"
+                   >
+                       {logOut} Logout
+                     </button>
+                   </div>
+                 ) : (
+                   <NavLink to="/login" className="navHover nav-link text-light">
+                     Login
+                   </NavLink>)}
+      </span>
+    </div>
+  </div>
+</nav>
            
            
             
